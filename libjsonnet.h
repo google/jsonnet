be55ed0d2e6a14ff52a0d3c5cdb26f63e57fc13a
Dave Cunningham <dcunnin@google.com>
2015-06-02 21:28:56 -0400

    Bump version number

--- libjsonnet.h
+++ libjsonnet.h
@@ -23,7 +23,7 @@ limitations under the License.
  */
 
 
-#define LIB_JSONNET_VERSION "v0.7.5"
+#define LIB_JSONNET_VERSION "v0.7.6"
 
 
 /** Return the version string of the Jsonnet interpreter.  Conforms to semantic versioning
