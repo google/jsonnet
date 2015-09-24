e00d5d1ea9a0ead8f004287e05c8e5f28a482b97
Dave Cunningham <dcunnin@google.com>
2015-09-23 22:41:53 -0400

    Colorize and make test script more user friendlt

--- test_suite/run_tests.sh
+++ test_suite/run_tests.sh
@@ -17,6 +17,15 @@
 EXECUTED=0
 FAILED=0
 
+# Enable next line to test the garbage collector
+#PARAMS="--gc-min-objects 1 --gc-growth-trigger 1"
+
+# Enable next line for a slow and thorough test
+#VALGRIND="valgrind -q"
+
+# Display successful as well as failed tests (useful if they are slow).
+VERBOSE=false
+
 for TEST in *.jsonnet ; do
     EXPECTED_EXIT_CODE=0
     GOLDEN="true"
@@ -30,33 +39,31 @@ for TEST in *.jsonnet ; do
     if [ -r "$TEST.golden_regex" ] ; then
         GOLDEN_REGEX=$(cat "$TEST.golden_regex")
     fi
-    OUTPUT="$(../jsonnet --gc-min-objects 1 --gc-growth-trigger 1 --var var1=test --code-var var2='{x:1, y: 2}' "$TEST" 2>&1 )"
+    OUTPUT="$($VALGRIND ../jsonnet $PARAMS --var var1=test --code-var var2='{x:1, y: 2}' "$TEST" 2>&1 )"
     EXIT_CODE=$?
     if [ $EXIT_CODE -ne $EXPECTED_EXIT_CODE ] ; then
         FAILED=$((FAILED + 1))
-        echo "FAIL (exit code): $TEST"
+        echo "[31;1mFAIL[0m [1m(exit code)[0m: [36m$TEST[0m"
         echo "Output:"
         echo "$OUTPUT"
     else
         if [ -n "$GOLDEN_REGEX" ] ; then
             if [[ ! "$OUTPUT" =~ $GOLDEN_REGEX ]] ; then
                 FAILED=$((FAILED + 1))
-                echo "FAIL (output regex mismatch): $TEST"
+                echo "[31;1mFAIL[0m [1m(regex mismatch)[0m: [36m$TEST[0m"
                 echo "Output:"
                 echo "$OUTPUT"
             else
-                true
-                #echo "SUCCESS: $TEST"
+                $($VERBOSE) && echo "[32mSUCCESS[0m: [36m$TEST[0m"
             fi
         else
             if [ "$OUTPUT" != "$GOLDEN" ] ; then
                 FAILED=$((FAILED + 1))
-                echo "FAIL (output mismatch): $TEST"
+                echo "[31;1mFAIL[0m [1m(output mismatch)[0m: [36m$TEST[0m"
                 echo "Output:"
                 echo "$OUTPUT"
             else
-                true
-                #echo "SUCCESS: $TEST"
+                $($VERBOSE) && echo "[32mSUCCESS[0m: [36m$TEST[0m"
             fi
         fi
     fi
