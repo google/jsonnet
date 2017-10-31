10c3416b854235ed01066a7291007163649dbd73
Dave Cunningham <sparkprime@gmail.com>
2017-10-30 21:53:05 -0400

    Some UX improvements to commandline tool + cmdline tests

---
+++ test_cmd/max_stack1.golden.stderr.go
@@ -0,0 +1,4 @@
+RUNTIME ERROR: Max stack frames exceeded.
+	<cmdline>:1:14-15	$
+	During evaluation	
+
---
+++ test_cmd/max_stack4.golden.stderr.go
@@ -0,0 +1,10 @@
+RUNTIME ERROR: Max stack frames exceeded.
+	<std>:979:29-30	thunk from <thunk <ta> from <function <anonymous>>>
+	<builtin>	builtin function <type>
+	<std>:981:33-35	thunk from <function <anonymous>>
+	<builtin>	builtin function <primitiveEquals>
+	<std>:981:12-40	function <anonymous>
+		
+	<cmdline>:1:69-75	$
+	During evaluation	
+
