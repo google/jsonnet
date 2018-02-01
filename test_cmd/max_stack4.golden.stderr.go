39ff6f7aaf5c8a3198e1a580d3ae1be69482cce5
Dave Cunningham <dcunnin@google.com>
2018-02-01 14:21:23 -0500

    Update golden for go-jsonnet

--- test_cmd/max_stack4.golden.stderr.go
+++ test_cmd/max_stack4.golden.stderr.go
@@ -1,9 +1,9 @@
 RUNTIME ERROR: Max stack frames exceeded.
-	<std>:979:29-30	thunk from <thunk <ta> from <function <anonymous>>>
+	<std>:1036:29-30	thunk from <thunk <ta> from <function <anonymous>>>
 	<builtin>	builtin function <type>
-	<std>:981:33-35	thunk from <function <anonymous>>
+	<std>:1038:33-35	thunk from <function <anonymous>>
 	<builtin>	builtin function <primitiveEquals>
-	<std>:981:12-40	function <anonymous>
+	<std>:1038:12-40	function <anonymous>
 		
 	<cmdline>:1:69-75	$
 	During evaluation	
