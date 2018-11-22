478c853f7addd37af800f1b93bf1b70a9004df45
Stanisław Barzowski <stanislaw.barzowski@gmail.com>
2018-11-22 16:49:43 +0100

    Add std.encodeUTF8, std.decodeUTF8

--- core/desugarer.cpp
+++ core/desugarer.cpp
@@ -34,7 +34,7 @@ struct BuiltinDecl {
     std::vector<UString> params;
 };
 
-static unsigned long max_builtin = 35;
+static unsigned long max_builtin = 37;
 BuiltinDecl jsonnet_builtin_decl(unsigned long builtin)
 {
     switch (builtin) {
@@ -74,6 +74,8 @@ BuiltinDecl jsonnet_builtin_decl(unsigned long builtin)
         case 33: return {U"asciiUpper", {U"str"}};
         case 34: return {U"join", {U"sep", U"arr"}};
         case 35: return {U"parseJson", {U"str"}};
+        case 36: return {U"encodeUTF8", {U"str"}};
+        case 37: return {U"decodeUTF8", {U"arr"}};
         default:
             std::cerr << "INTERNAL ERROR: Unrecognized builtin function: " << builtin << std::endl;
             std::abort();
---
+++ test_suite/error.decodeUTF8_nan.jsonnet
@@ -0,0 +1,1 @@
+std.decodeUTF8(['foo'])
