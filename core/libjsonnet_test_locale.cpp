9ee4e2c2b5e5bcc5f9e1fdec2256712c77a4481f
John Bartholomew <jpab@google.com>
2024-02-29 16:55:32 +0000

    Fix locale test to avoid relying on test system locales

--- core/libjsonnet_test_locale.cpp
+++ core/libjsonnet_test_locale.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <sstream>
 #include <locale>
 #include <cassert>
 #include <libjsonnet++.h>
 
 // Regression test for the follwing issue: https://github.com/google/jsonnet/issues/722
 
+// Define a custom numpunct facet, so that we don't rely on any particular locale
+// being available on the system where the test is built and run.
+class Punctuator : public std::numpunct<char> {
+protected:
+    virtual char do_decimal_point() const override { return '!'; }
+    virtual std::string do_grouping() const override { return "\1\2"; }
+    virtual char do_thousands_sep() const override { return '\''; }
+};
+
 int main() {
-    std::string templatedJSONString { "2000" };
-    std::locale glocale("en_US.UTF-8"); 
+    std::string templatedJSONString { "20000.5" };
+    Punctuator punctuator;
+    std::locale glocale(std::locale::classic(), &punctuator);
     std::locale::global(glocale);
+
+    // Self-test to make sure the custom global locale is actually being applied.
+    std::ostringstream ss;
+    ss << 20000.5;
+    assert(ss.str() == "20'00'0!5");
 
     jsonnet::Jsonnet jsonnet {};
     jsonnet.init();
@@ -18,7 +34,7 @@ int main() {
         std::cerr << "Error parsing Jsonnet: "+jsonnet.lastError();
         exit(1);
     }
-    std::string expected = "2000\n";
+    std::string expected = "20000.5\n";
     assert(expected == expanded);
     return 0;
 }
