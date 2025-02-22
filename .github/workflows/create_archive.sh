2eccc0ae7f799fab12bb6759c606116599c8190a
John Bartholomew <jpa.bartholomew@gmail.com>
2025-02-22 15:37:28 +0000

    ci: allow building from prepare-release without adding git hash suffix
    Gradually trying to work out a reasonable release workflow. Something
    like:
    
    - Update version number etc on branch "prepare-release"
    - Trigger release workflow: This generates a source archive and a draft
      GitHub release object. The GitHub release has a propsed tag that
      comes from the version in libjsonnet.h.
    - Trigger Python publish workflow: This generates Python packages,
      then waits for approval before publishing to PyPI.
    - Fill in release notes in GitHub. Do any last minute checks.
      Consider actually downloading the release artifacts and checking
      them locally.
    - Approve the Python publish workflow.
    - Publish the GitHub release (which will create the version tag)
    - Fast-forward master to the new version.
    
    It's a little ugly, but the benefits here are:
    - All the final build artifacts are generated before any publishing
      so they can be checked (manually or otherwise).
    - Release notes can be edited in GitHub.
    - The tag is created at publish time.

--- .github/workflows/create_archive.sh
+++ .github/workflows/create_archive.sh
@@ -15,12 +15,14 @@
 
 set -o errexit -o nounset -o pipefail
 
-# Set by GH actions, see
-# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 JSONNET_VERSION="$(grep -Ee '^\s*#\s*define\s+LIB_JSONNET_VERSION\s+"[^"]+"\s*$' include/libjsonnet.h | sed -E -e 's/[^"]+"([^"]+)".*/\1/')"
 
+# GITHUB_REF is set by GH actions, see
+# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+
 VERSION_SUFFIX=
-if [[ "${GITHUB_REF_TYPE}" != 'tag' || "${GITHUB_REF_NAME}" != "${JSONNET_VERSION}" ]]; then
+if [[ ( "${GITHUB_REF_TYPE}" != 'branch' || "${GITHUB_REF_NAME}" != "prepare-release" ) &&
+      ( "${GITHUB_REF_TYPE}" != 'tag' || "${GITHUB_REF_NAME}" != "${JSONNET_VERSION}" ) ]]; then
     >&2 echo 'WARNING: Jsonnet library version in header does not match release ref. Adding commit suffix.'
     VERSION_SUFFIX="-${GITHUB_SHA:0:9}"
 fi
@@ -34,31 +36,3 @@ ARCHIVE_SHA=$(shasum -a 256 "$ARCHIVE" | awk '{print $1}')
 echo "archive_sha256=${ARCHIVE_SHA}" >> "$GITHUB_OUTPUT"
 echo "jsonnet_version=${JSONNET_VERSION}" >> "$GITHUB_OUTPUT"
 echo "jsonnet_version_permanent=${JSONNET_VERSION}${VERSION_SUFFIX}" >> "$GITHUB_OUTPUT"
-
-cat > bazel_dep_release_notes.txt << EOF
-### Importing Jsonnet in a project that uses Bazel
-
-#### Using Bzlmod with Bazel 6
-
-Add to your \`MODULE.bazel\` file:
-
-\`\`\`starlark
-bazel_dep(name = "jsonnet", version = "${JSONNET_VERSION}")
-\`\`\`
-
-#### Using WORKSPACE
-
-Paste this snippet into your \`WORKSPACE\` file:
-
-\`\`\`starlark
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-
-http_archive(
-    name = "jsonnet",
-    sha256 = "${ARCHIVE_SHA}",
-    strip_prefix = "${PREFIX}",
-    url = "https://github.com/google/jsonnet/releases/download/${JSONNET_VERSION}/${ARCHIVE}",
-)
-\`\`\`
-
-EOF
